360b9c738eeee9f7806a8e114cdb3aa90edba825
Justin Farooq <justinfarooq@gmail.com>
2014-11-22 18:12:28 -0800

    arg1

--- Trello-clone/app/assets/javascripts/trello_clone.js
+++ Trello-clone/app/assets/javascripts/trello_clone.js
@@ -8,5 +8,6 @@ window.TrelloClone = {
     // pass in a rootEl to router, not as an object
   	new TrelloClone.Routers.AppRouter($("#main"));
   	Backbone.history.start();
+    console.log("meh"); //delete me
   }
 };
